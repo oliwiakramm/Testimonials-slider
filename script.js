32e4349c76ac6b14aa728780a943afd6992bcd3c
oliwiakramm <oliwiakramm@gmail.com>
2023-04-10 13:42:10 +0200

    functionality added

--- script.js
+++ script.js
@@ -4,27 +4,45 @@ const nextBtn = document.getElementById("nextBtn");
 
 let index = 0;
 
-prevBtn.addEventListener("click", function () {
-  index--;
-
-  if (index < 0) {
-    index = testimonials.length - 1;
-  }
-  showTestimonial(index);
-});
-
-nextBtn.addEventListener("click", function () {
-  index++;
-
-  if (index > testimonials.length - 1) {
-    index = 0;
-  }
-  showTestimonial(index);
-});
-
 function showTestimonial(i) {
   testimonials.forEach((testimonial) => {
     testimonial.classList.remove("active");
   });
   testimonials[i].classList.add("active");
 }
+
+function indexMin() {
+  index--;
+
+  if (index < 0) {
+    index = testimonials.length - 1;
+  }
+  showTestimonial(index);
+}
+
+function indexMax() {
+  index++;
+
+  if (index > testimonials.length - 1) {
+    index = 0;
+  }
+  showTestimonial(index);
+}
+
+prevBtn.addEventListener("click", function () {
+  indexMin();
+});
+
+nextBtn.addEventListener("click", function () {
+  indexMax();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key == "ArrowLeft") {
+    indexMin();
+  }
+
+  if (e.key == "ArrowRight") {
+    indexMax();
+  }
+});
